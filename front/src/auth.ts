53a48156d6288f36bccc07b864f4fae0fbe09537
takahiro-maekawa <koma4024@gmail.com>
2025-05-20 20:57:22 +0900

    #XXXX 初期プロジェクトの準備
    - Next.jsの初期プロジェクトを用意
    - Auth.jsの動作確認のため、サインインandサインアウトボタンを追加しました
    - #XXXX (課題名) をコミットメッセージに含めてください
    - その後は箇条書きでやったことの概要を書いていきます

---
+++ front/src/auth.ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth"
+import "next-auth/jwt"
+
+import Google from "next-auth/providers/google"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: !!process.env.AUTH_DEBUG,
+  theme: { logo: "https://authjs.dev/img/logo-sm.png" }, // ロゴを設定します
+  providers: [
+    Google,
+  ],
+  basePath: "/auth",
+  session: { strategy: "jwt" },
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname === "/sandbox") return true // ここでログインパスを制御できます。
+      // if (pathname === "/sandbox2") return !!auth
+      return true
+    },
+    async session({ session, token }) {
+      if (token?.accessToken) session.accessToken = token.accessToken
+
+      return session
+    },
+  },
+  experimental: { enableWebAuthn: true },
+})
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
---
+++ front/src/components/auth-components.tsx
@@ -0,0 +1,34 @@
+import { signIn, signOut } from "@/auth"
+
+export function SignIn({
+  provider,
+  ...props
+}: { provider?: string } & React.ComponentPropsWithRef<"button">) {
+  return (
+    <div className="bg-blue-500 p-2">
+      <form
+        action={async () => {
+          "use server"
+          await signIn(provider)
+        }}
+      >
+        <button {...props}>Sign In</button>
+      </form>
+    </div>
+  )
+}
+
+export function SignOut(props: React.ComponentPropsWithRef<"button">) {
+  return (
+    <div className="bg-red-500 p-2">
+      <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <button {...props}>Sign Out</button>
+      </form>
+    </div>
+  )
+}
