9d104add25e69e4b394d204c17868d3e45a26788
atomisu0312 <koma4024@gmail.com>
2025-05-29 14:28:25 +0900

    【共通】libsとlibが混在している #28
    - libs→libに移行
    - shadcnのデフォルトlibパッケージを移動
    - 【その他】サイドメニューとヘッダー #24追加分に影響が出る
    - その他authロジックのimport箇所が変更される

--- front/src/components/organisms/auth/authComponents.tsx
+++ front/src/components/organisms/auth/authComponents.tsx
@@ -1,4 +1,4 @@
-import { signIn, signOut } from "@/libs/auth/auth"
+import { signIn, signOut } from "@/lib/auth/auth"
 
 export function SignIn({
   provider,
--- front/src/components/templates/common/header.tsx
+++ front/src/components/templates/common/header.tsx
@@ -1,5 +1,5 @@
 import { SignIn, SignOut } from "@/components/organisms/auth/authComponents";
-import { auth } from "@/libs/auth/auth";
+import { auth } from "@/lib/auth/auth";
 
 export default async function Header() {
   const session = await auth();
