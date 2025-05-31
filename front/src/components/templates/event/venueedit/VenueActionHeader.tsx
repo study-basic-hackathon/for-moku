d027ad7cf8bf917b0803e81b1b037dc4d9a095b0
atomisu0312 <koma4024@gmail.com>
2025-05-31 19:13:22 +0900

    【イベント】【間取り編集】ツールの仕様決めやサンプル画面の作成などの下準備 #44
    ヘッダの微調整

--- front/src/components/templates/event/venueedit/VenueActionHeader.tsx
+++ front/src/components/templates/event/venueedit/VenueActionHeader.tsx
@@ -1,8 +1,8 @@
 export default function VenueActionHeader() {
   return (
     <div className="flex flex-col justify-center items-center border border-gray-900 mx-2 rounded-lg p-4 m-4 gap-4">
-      <div className="items-center justify-between w-full grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-2">
-        <div className="col-span-1 lg:col-span-3 xl:col-span-1 flex justify-between mx-2">
+      <div className="items-center justify-between w-full grid grid-cols-4 lg:grid-cols-4 xl:grid-cols-2">
+        <div className="col-span-3 xl:col-span-1 flex justify-between mx-2">
           <div className="flex space-x-2">
             <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
               元に戻す
