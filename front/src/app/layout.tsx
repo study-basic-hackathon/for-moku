f2dff887c7b50914fadb10f78852f012066b7f45
01063 <brianhaifan.xu@gmail.com>
2025-06-07 17:36:02 +0900

    fix layout: change height of main to account for top-14 property

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex`}
       >
         <TopNav />
-        <main className="fixed top-14 overflow-y-auto w-full h-full">
+        <main className="fixed top-14 overflow-y-auto w-full h-[calc(100vh-56px)]">
           {children}
         </main>
       </body>
