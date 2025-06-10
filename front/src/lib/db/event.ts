7b4ec5041738022641c0f4ff7964f47e60a36aff
atomisu0312 <koma4024@gmail.com>
2025-06-11 00:27:50 +0900

    【イベント】【新規作成】イベント新規作成時に項目を入力して永続化できるようにする #108
    - 【重要】テーブル定義の変更
    - スキーマ定義を作成
    - スキーマ定義に従いlib/db以下を作成
    - リダイレクト処理そのものとリダイレクト先のページを作成(データはモック)
    
    テーブル定義について
    - 時刻なのか日時なのかの判別がつかなかったのでstart_time -> start_date_time
    - 同様の理由でend_time -> end_date_time
    - 一般的に推奨されているので、間取りJSONの型をJSON -> JSONB
    
    その他
    - FormSelectのmoleculesの微修正

---
+++ front/src/lib/db/event.ts
@@ -0,0 +1,54 @@
+import { db } from "@/lib/db";
+import { events } from "@/lib/db/schema/event";
+import { eq, ExtractTablesWithRelations} from "drizzle-orm";
+import { NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
+import { PgTransaction } from "drizzle-orm/pg-core";
+import * as schema from '@/lib/db/schema';
+import { Event, NewEvent, UpdateEvent } from "@/types/event/schema";
+
+// ToDO後で消す
+export type Transaction = PgTransaction<NodePgQueryResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>;
+
+/**
+ * イベントを登録する
+ * @param event 登録するイベント情報
+ * @returns 登録されたイベント
+ */
+export async function insertEvent(tx: Transaction, event: NewEvent): Promise<Event> {
+  const [createdEvent] = await tx.insert(events).values(event).returning();
+  return createdEvent;
+}
+
+/**
+ * イベントを取得する
+ * @param id イベントID
+ * @returns イベント情報
+ */
+export async function selectEventById(id: number): Promise<Event | undefined> {
+  const [event] = await db.select().from(events).where(eq(events.id, id));
+  return event;
+}
+
+/**
+ * グループに所属するイベント一覧を取得する
+ * @param userGroupId ユーザーグループID
+ * @returns イベント一覧
+ */
+export async function selectEventsByUserGroupId(userGroupId: number): Promise<Event[]> {
+  return await db.select().from(events).where(eq(events.userGroupId, userGroupId));
+}
+
+/**
+ * イベントを更新する
+ * @param id イベントID
+ * @param event 更新するイベント情報
+ * @returns 更新されたイベント
+ */
+export async function updateEvent(tx: Transaction, id: number, event: UpdateEvent): Promise<Event | undefined> {
+  const [updatedEvent] = await tx
+    .update(events)
+    .set({ ...event, updatedAt: new Date() })
+    .where(eq(events.id, id))
+    .returning();
+  return updatedEvent;
+}
