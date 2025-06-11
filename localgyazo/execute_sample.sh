37ad6febfcf5ff2ce43249b94b9e5402dbc82e30
atomisu0312 <koma4024@gmail.com>
2025-06-11 13:43:29 +0900

    Revert "【共通】ローカル用のGyazoAPIをdockerで用意する #135"
    This reverts commit fd40e1e0f2f9d23c8bd0f4456fb57bb29b08db00.

--- localgyazo/execute_sample.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ACCESS_TOKEN=sampletoken
-IMAGE_PATH=./sample_img/test.png
-LOCAL_END_POINT=http://localhost:8888/upload.cgi
-
-curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -F "imagedata=@$IMAGE_PATH" "$LOCAL_END_POINT"
