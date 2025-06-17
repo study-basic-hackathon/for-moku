28081a5d4b223e43873edad75d8fe9a2c10731c6
01063 <brianhaifan.xu@gmail.com>
2025-06-17 15:51:05 +0900

    【共通】404エラーファイル #181

---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+    <div className="size-full flex flex-col justify-center items-center">
+      <h1 className="text-[12rem]/[12rem]">404</h1>
+      <p className="text-lg">PAGE NOT FOUND</p>
+    </div>
+	)
+}
