4a903154221f256aded82f9bae968879164f98ec
01063 <brianhaifan.xu@gmail.com>
2025-06-22 09:12:38 +0900

    add api/room route

---
+++ front/src/app/api/room/[roomId]/page.tsx
@@ -0,0 +1,31 @@
+import { selectEventById } from "@/lib/db/event";
+import { NextRequest, NextResponse } from "next/server";
+
+ /**
+ * @param req リクエスト(画像データ)
+ * @returns レスポンス(Gyazo APIからのレスポンス)
+ */
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ roomId: string }> },
+) {
+  const { roomId } = await params;
+  const event = await selectEventById(Number(roomId));
+  console.log(roomId, event);
+  return NextResponse.json({ message: event!.endDateTime });
+}
+
+ /**
+ * @param req リクエスト(画像データ)
+ * @returns レスポンス(Gyazo APIからのレスポンス)
+ */
+export async function POST(
+  req: NextRequest,
+  { params }: { params: Promise<{ roomId: string }> },
+) {
+
+  console.log("post request");
+  const test = await req.json();
+  console.log(test);
+  return NextResponse.json({ message: "post request success", status: 201 });
+}
