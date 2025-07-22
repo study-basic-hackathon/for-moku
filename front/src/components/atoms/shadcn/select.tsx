0ccef3edbfc7f8d6bacfb9faeee6089804c4430e
atomisu0312 <55617583+atomisu0312@users.noreply.github.com>
2025-07-22 21:05:34 +0900

    【共通】【Form用汎用コンポーネント】Selectするフィールド用のコンポーネントを実装 #254 (#255)
    汎用Form機能において、select系のフィールドであればがそのまま使われていました。
    しかしながら、今のままでは、見た目がブラウザに依存してしまいます。
    また、#250 で実装されたものと見た目が微妙に異なるため、若干違和感がありました(@イベント新規登録画面)。
    
    そこで、FormSelect.tsxにおいて、HTML標準をそのまま使うのではなく、shadcnで実装されているSelectをラッピングするように変更しました。
    
    この変更により、一覧系画面にある、表示件数選択ボタンの見た目が若干変わります。

--- front/src/components/atoms/shadcn/select.tsx
+++ front/src/components/atoms/shadcn/select.tsx
@@ -44,7 +44,7 @@ function SelectTrigger({
     >
       {children}
       <SelectPrimitive.Icon asChild>
-        <ChevronDownIcon className="size-4 opacity-50" />
+        <ChevronDownIcon className="size-4 opacity-100 text-gray-900" />
       </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
   )
